41a655adb3a6d0e423a7047971f0e550418c8df1
Bekhzod7799 <124039801+Bekhzod7799@users.noreply.github.com>
2023-04-18 20:56:47 +0500

    Update RoomType.cs
    RoomTypes changed

--- TestHotel/TestHotel.DataAccess/Model/RoomType.cs
+++ TestHotel/TestHotel.DataAccess/Model/RoomType.cs
@@ -8,11 +8,11 @@ namespace TestHotel.DataAccess.Model
 {
     internal class RoomType
     {
-        public string RoomTypes { get; set; }
+        public int RoomTypesID { get; set: }
+        public enum RoomTypes { get; set; }
         public int RoomPrice { get; set; }
-        public int DefaultRoomPrice { get; set; }
         public string RoomImg { get; set; }
-        public string RoomsDesc { get; set; }
+        public string RoomsDescription { get; set; }
 
         public Room Room { get; set; }
     }
