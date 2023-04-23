0991b1f45911bf2d24ace692658e626915312f0a
sher_violino1989@list.ru <sher_violino1989@list.ru>
2023-04-24 00:29:46 +0500

    Added DbContext

---
+++ TestHotel/TestHotel.DataAccess/DbConnection/HotelDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestHotel.DataAccess.Model;
+
+namespace TestHotel.DataAccess.DbConnection
+{
+    public class HotelDbContext : DbContext
+    {
+        public HotelDbContext(DbContextOptions<HotelDbContext> options) :
+            base(options)
+        {
+        }
+
+        DbSet<Bill> Bills { get; set; }
+
+        DbSet<Booking> Bookings { get; set; }
+
+        DbSet<Employee> Employees { get; set; }
+
+        DbSet<Guest> Guests { get; set; }
+
+        DbSet<Hotel> Hotels { get; set; }
+
+        DbSet<Role> Roles { get; set; }
+
+        DbSet<Room> Rooms { get; set; }
+
+        DbSet<RoomType> RoomTypes { get; set; }
+    }
+}
