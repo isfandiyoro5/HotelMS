731851b73ab950658e345f71c037209da7f82da7
MirazamTUT <mmiromonov111@gmail.com>
2023-05-02 00:44:54 +0500

    fixed errors

--- TestHotel/TestHotel.DataAccess/Model/Bill.cs
+++ TestHotel/TestHotel.DataAccess/Model/Bill.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace TestHotel.DataAccess.Model
 {
     public class Bill
     {
+        [Required]
+        [Key]
         public int InvoiceNumber { get; set; }
 
         public int BookingId { get; set; }
--- TestHotel/TestHotel.DataAccess/Model/Room.cs
+++ TestHotel/TestHotel.DataAccess/Model/Room.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace TestHotel.DataAccess.Model
 {
     public class Room
     {
+        [Required]
+        [Key]
         public int RoomNumber { get; set; }
 
         public RoomTypes RoomTypes { get; set; }
