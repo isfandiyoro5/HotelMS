1e65c3b851f5defa5527bef28b36ab85ad0b61cf
MirazamTUT <mmiromonov111@gmail.com>
2023-05-01 22:59:54 +0500

    added CRUD

---
+++ TestHotel/TestHotel.API/Controller/EmployeeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestHotel.DataAccess.Repository.IRepositories;
+using TestHotel.DataAccess.Model;
+using System.Runtime.CompilerServices;
+
+namespace TestHotel.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpPost]
+        public async Task<int> CreateEmployeeAsync(Employee employee)
+        {
+            return await _employeeRepository.AddEmployeeAsync(employee);
+        }
+
+        [HttpGet("id")]
+        public async Task<Employee> GetEmployeeByIdAsync(int id)
+        {
+            return await _employeeRepository.GetEmployeeByIdAsync(id);
+        }
+
+        [HttpGet]
+        public async Task<List<Employee>> GetAllEmployeesAsync()
+        {
+            return await _employeeRepository.GetAllEmployeesAsync();
+        }
+
+        [HttpPut]
+        public async Task<int> UpdateEmployeeAsync(Employee employee)
+        {
+            return await _employeeRepository.UpdateEmployeeAsync(employee);
+        }
+
+        [HttpDelete]
+        public async Task<int> DeleteEmployeeAsync(Employee employee)
+        {
+            return await _employeeRepository.DeleteEmployeeAsync(employee);
+        }
+    }
+}
--- TestHotel/TestHotel.API/Program.cs
+++ TestHotel/TestHotel.API/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 using TestHotel.DataAccess.DbConnection;
+using TestHotel.DataAccess.Repository.IRepositories;
+using TestHotel.DataAccess.Repository.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 var logger = new LoggerConfiguration()
@@ -14,6 +16,15 @@ builder.Services.AddControllers().AddNewtonsoftJson(op => op.SerializerSettings.
 
 var config = builder.Configuration.GetSection("ConnectionStrings");
 builder.Services.AddDbContext<HotelDbContext>(option => option.UseNpgsql(config["Connect"]));
+builder.Services.AddScoped<IBillRepository, BillRepository>();
+builder.Services.AddScoped<IBookingRepository, BookingRepository>();
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IGuestRepository, GuestRepository>();
+builder.Services.AddScoped<IHotelRepository, HotelRepository>();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+builder.Services.AddScoped<IRoomRepository, RoomRepository>();
+builder.Services.AddScoped<IRoomTypeRepository, RoomTypeRepository>();
+builder.Services.AddScoped<HotelDbContext>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
