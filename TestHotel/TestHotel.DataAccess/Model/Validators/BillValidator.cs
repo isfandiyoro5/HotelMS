b424600b4c30026c226d4202675b3fc204e9b92d
MirazamTUT <mmiromonov111@gmail.com>
2023-05-11 14:27:16 +0500

    updated validators

---
+++ TestHotel/TestHotel.DataAccess/Model/Validators/BillValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace TestHotel.DataAccess.Model.Validators
+{
+    public class BillValidator : AbstractValidator<Bill>
+    {
+        public BillValidator()
+        {
+        }
+    }
+}
