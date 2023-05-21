13cb58ed06509b30a6d0cfe0efd178cf70581bb7
MirazamTUT <mmiromonov111@gmail.com>
2023-05-21 13:45:46 +0500

    Updated registrations in program.cs

--- TestHotel/TestHotel.API/Program.cs
+++ TestHotel/TestHotel.API/Program.cs
@@ -1,11 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 using TestHotel.DataAccess.DbConnection;
-using AutoMapper;
 using Microsoft.Extensions.DependencyInjection;
 using TestHotel.Service.DTO.AutoMapper;
 using TestHotel.Service.DTO.RequestDto;
-using FluentValidation;
+using TestHotel.Service.ServiceExtensions;
 
 var builder = WebApplication.CreateBuilder(args);
 var logger = new LoggerConfiguration()
@@ -19,16 +18,7 @@ builder.Services.AddControllers().AddNewtonsoftJson(op => op.SerializerSettings.
 
 var config = builder.Configuration.GetSection("ConnectionStrings");
 builder.Services.AddDbContext<HotelDbContext>(option => option.UseNpgsql(config["Connect"]));
-builder.Services.AddMvc();
-builder.Services.AddScoped<IValidator<BillRequestDto>, BillRequestDtoValidator>();
-builder.Services.AddScoped<IValidator<BookingRequestDto>, BookingRequestDtoValidator>();
-builder.Services.AddScoped<IValidator<EmployeeRequestDto>, EmployeeRequestDtoValidator>();
-builder.Services.AddScoped<IValidator<GuestRequestDto>, GuestRequestDtoValidator>();
-builder.Services.AddScoped<IValidator<HotelRequestDto>, HotelRequestDtoValidator>();
-builder.Services.AddScoped<IValidator<RoleRequestDto>, RoleRequestDtoValidator>();
-builder.Services.AddScoped<IValidator<RoomRequestDto>, RoomRequestDtoValidator>();
-builder.Services.AddScoped<IValidator<RoomTypeRequestDto>, RoomTypeRequestDtoValidator>();
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.RegisterServices(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
