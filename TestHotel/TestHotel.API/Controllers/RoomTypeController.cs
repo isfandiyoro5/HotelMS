ab29aa3fa91f68250e577c67f65dae28ca56b97c
sher_violino1989@list.ru <sher_violino1989@list.ru>
2023-05-31 03:11:53 +0500

    Id qowildi

--- TestHotel/TestHotel.API/Controllers/RoomTypeController.cs
+++ TestHotel/TestHotel.API/Controllers/RoomTypeController.cs
@@ -68,7 +68,7 @@ namespace TestHotel.API.Controllers
             }
         }
 
-        [HttpDelete]
+        [HttpDelete("Id")]
         public async Task<ActionResult<int>> DeleteRoomType(int id)
         {
             try
